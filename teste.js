44b03ced12f5ef103a7d77785e19a5b73129aa52
NathanReis <nathanferreirareis_13@hotmail.com>
2025-11-29 21:21:14 -0300

    correção

--- teste.js
+++ teste.js
@@ -2,11 +2,11 @@ import fs from "fs";
 import lib from "@joseiran/svguid-a3";
 
 // String contendo SVG criado pela lib
-let svgText = lib.getSVG("Amorin");
+let svgText = lib.getSVG("Amorim");
 
 // Cria o arquivo SVG
 fs.writeFileSync("Saida.svg", svgText.trim());
 
-console.log("Arquivo SVG criado com sucesso: output.svg");
+console.log("Arquivo SVG criado com sucesso: Saida.svg");
 // Mostra o conteúdo do SVG no console
 console.log(svgText);
